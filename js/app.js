7f4e129537c34aeb810ab8a076cb5b12fc8dbfbb
Klaus Badelt <klaus@kinonation.com>
2014-02-03 16:39:49 -0800

    scrolling works in blog sidenav

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
-var $root = $('html, body');
-$('a').click(function(e) {
-    var href = $.attr(this, 'href');
-    $root.animate({
-        scrollTop: $(href).offset().top - 50
-    }, 500, function () {
-        window.location.hash = href;
-    });
-    // $('.nav li').removeClass('active');
-    // var $parent = $(this).parent();
-    // if (!$parent.hasClass('active')) {
-    //   $parent.addClass('active');
-    // }      
-    e.preventDefault();
+$(document).ready(function() {
+  var $root = $('html, body');
+  $('a.animate').click(function(e) {
+      var href = $.attr(this, 'href');
+      $root.animate({
+          scrollTop: $(href).offset().top - 50
+      }, 500, function () {
+          window.location.hash = href;
+      });
+      e.preventDefault();
+  });  
 });

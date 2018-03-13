e172fb74745c61eff41a3b551ba3a930c9542e9f
Klaus Badelt <info@klausbadelt.com>
2018-03-13 15:17:11 +1100

    Improving Request-a-Demo Slack message - catalog size, formatting, UX

--- js/form2slack.js
+++ js/form2slack.js
@@ -1,28 +1,48 @@
 // send contact form to Slack
 function sendForm2Slack() {
-  $('#inputSubmit').html("<i class='fa fa-spinner fa-spin'>");
+  $('#inputSubmit').html("<i class='fa fa-spinner fa-spin'>").attr('disabled',true);
+  $("#requestdemomodal .modal-body").after(
+    '<div class="modal-footer">' +
+    '  <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>' +
+    '</div>');
   var name = $("#inputName").val();
   var email = $("#inputEmail").val();
   var phone = $("#inputPhone").val();
-  var text = 'Another demo request from our website!\nFirst name: ' + name + '\n' +
+  var company = $("#inputCompany").val() || "(not specified)";
+  var country = $("#inputCountry").val() || "(not specified)";
+  var catalog = $('input[name=inputCatalog]:checked', '#requestdemoform').val() || "(not specified)";
+  var fallback =
+    'Yay - another demo request, <@roger>!\n' +
+    'Name: ' + name + '\n' +
     'Email: ' + email + '\n' +
-    'Phone: ' + phone;
-  var closeBtn =
-    '<div class="modal-footer"> \
-      <button type="button" class="btn btn-default" data-dismiss="modal">\
-        Close\
-      </button>\
-    </div>';
+    'Phone: ' + phone + '\n' +
+    'Company: ' + company + '\n' +
+    'Country: ' + country + '\n' +
+    'Catalog Size: ' + catalog;
+  var message = {
+    "attachments": [{
+      "fallback": fallback,
+      "pretext": "Yay - another demo request, <@U055UT33L>",
+      "fields": [
+        { "title": "Name",         "value": name },
+        { "title": "Email",        "value": email },
+        { "title": "Phone",        "value": phone },
+        { "title": "Company",      "value": company },
+        { "title": "Country",      "value": country },
+        { "title": "Catalog Size", "value": catalog },
+      ]
+    }]
+  };
   $.ajax({
     method: 'POST',
     url: "https://hooks.slack.com/services/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",
-    data: JSON.stringify({ "text": text }),
+    data: JSON.stringify(message),
     dataType: 'text',
     processData: false,
-  }).done(function(data)  {
-      $("#requestdemomodal .modal-body").html("Thank you! The request was successfully sent. We'll contact you asap.").after(closeBtn);
+  }).done(function()  {
+      $("#requestdemomodal .modal-body").html("Thank you! The request was successfully sent. We'll contact you asap.");
   }).fail(function()  {
-      $("#requestdemomodal .modal-body").html("Sorry - there was an error. Please reload and try again.").after(closeBtn);
+      $("#requestdemomodal .modal-body").html("Sorry - there was an error. Please reload and try again.");
   }).complete(function() {
       $("#requestdemobtn").hide();
   });
@@ -33,4 +53,4 @@ $(document).ready(function() {
     event.preventDefault();
     sendForm2Slack();
   });
-})
+});

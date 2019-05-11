9e568961520757a30689b4d14340de7008720d4e
Klaus Badelt <info@klausbadelt.com>
2019-05-11 21:37:50 +0100

    eliminate ESlint warnings

--- src/list-signup.js
+++ src/list-signup.js
@@ -1,50 +1,48 @@
-import client from '@sendgrid/client';
-import { parse } from "querystring";
+import client from '@sendgrid/client'
+import { parse } from 'querystring'
 
-function addSendgridRecipient(client, email, type) {
+function addSendgridRecipient (client, email, type) {
   return new Promise((resolve, reject) => {
     const data = [
       {
         email: email,
         type: type
       }
-    ];
+    ]
     const request = {
-      method: "POST",
-      url: "/v3/contactdb/recipients",
+      method: 'POST',
+      url: '/v3/contactdb/recipients',
       body: data
-    };
-    console.log('addSendgridRecipient request', request);
-    client.request( request )
-      .then( ([response, body]) => resolve(response) )
-      .catch( err => reject(err) );
-  });
+    }
+    console.log('addSendgridRecipient request', request)
+    client.request(request)
+      .then(([response, body]) => resolve(response))
+      .catch(err => reject(err))
+  })
 }
 
 // netlify-lambda has touble with async handler, keeping callback for now
-exports.handler = function(event, context, cb) {
+exports.handler = function (event, context, done) {
   try {
-    if (event.httpMethod !== "POST") {
-      cb({ statusCode: 405, body: 'Method Not Allowed' });
-      return;
+    if (event.httpMethod !== 'POST') {
+      done({ statusCode: 405, body: 'Method Not Allowed' })
+      return
     };
     if (!process.env.SENDGRID_API_KEY) {
-      cb({ statusCode: 401, body: 'API key missing' });
-      return;
+      done({ statusCode: 401, body: 'API key missing' })
+      return
     };
 
-    const body = parse(event.body);
-    client.setApiKey(process.env.SENDGRID_API_KEY);
+    const body = parse(event.body)
+    client.setApiKey(process.env.SENDGRID_API_KEY)
     addSendgridRecipient(client, body.email, body.type)
       .then( response => cb( null, { statusCode: 200, body: JSON.stringify(response) }) )
       .catch(err => {
         console.log('handler .catch', err)
-        cb(null, { statusCode: 422, body: JSON.stringify(err) });
+        cb(null, { statusCode: 422, body: JSON.stringify(err) })
       });
   } catch(err) {
-    console.log('handler catch block', err);
-    cb(null, { statusCode: 422, body: JSON.stringify(err) });
+    console.log('handler catch block', err)
+    cb(null, { statusCode: 422, body: JSON.stringify(err) })
   }
-};
-
-
+}

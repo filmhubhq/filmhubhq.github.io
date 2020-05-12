86fc0342bfd4df364470f8efffef5b1f36765509
Klaus Badelt <info@klausbadelt.com>
2020-05-11 18:58:18 -0700

    Update demo request

--- js/demoreq.js
+++ js/demoreq.js
@@ -38,8 +38,7 @@ function reqDemo() {
     dataType: 'text',
     processData: false,
   }).done(function()  {
-    $("#requestdemomodal .modal-body").html("Thank you! The request was successfully sent. We'll contact you asap.");
-    $("#requestdemomodal .modal-footer").html('<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>');
+    window.location.replace("https://cal.mixmax.com/alandescragnolle-305/demo");
   }).fail(function()  {
     $("#requestdemomodal .modal-body").html("Sorry - there was an error. Please reload and try again.");
     $("#requestdemomodal .modal-footer").html('<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>');
